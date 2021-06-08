ebae1d117f86512275ef780057dca5dae40e9632
丸山 司 <s14216@ca-20013397.cyberagent.co.jp>
2021-06-08 19:37:40 +0900

    add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const fizzbuzz = require('./libs/FizzBazz')
+const Add = require('./libs/Add')
 
 app.get('/', (req, res) => {
     res.send('Hello World\n')
@@ -12,6 +13,12 @@ app.get('/fizzbuzz/:num', (req, res) => {
     const num = req.params.num
     const resStr = fizzbuzz(Number(num))
     res.send(resStr)
+})
+
+app.get('/add/:num1/:num2', (req, res) => {
+    const {num1, num2} = req.params
+    const sum = add(Number(num1), Number(nu2))
+    res.send(sum)
 })
 
 app.listen(port, () => {
